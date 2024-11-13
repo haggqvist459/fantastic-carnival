64d997a98af9ff7a3c0cc996e9be15678fcaf5be
haggqvist459 <48819082+haggqvist459@users.noreply.github.com>
2024-11-13 17:00:28 +0100

    tutorial completed

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 // import { useState, useEffect } from 'react'
-import { useParams, useLoaderData, Link } from 'react-router-dom';
+import { useParams, useLoaderData, Link, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
 
     const { id } = useParams();
     const job = useLoaderData();
+    const navigate = useNavigate();
 
+    const onDeleteClick = (jobId) => {
+        const confirmDelete = window.confirm('You are about to delete a job listing. Are you sure?');
+        
+        if(!confirmDelete) return; 
+
+        deleteJob(jobId);
+        navigate('/jobs');
+
+    }
     // const [job, setJob] = useState(null);
     // const [loading, setLoading] = useState(true)
 
@@ -99,12 +109,13 @@ const JobPage = () => {
                             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                                 <Link
-                                    to={`/jobs/edit/${job.id}`}
+                                    to={`/edit-job/${job.id}`}
                                     className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                                 >Edit Job
                                 </Link>
                                 <button
                                     className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                                    onClick={() => onDeleteClick(job.id)}
                                 >
                                     Delete Job
                                 </button>
@@ -115,7 +126,7 @@ const JobPage = () => {
             </section>
 
         </>
-    )
+    ) 
 }
 
 
